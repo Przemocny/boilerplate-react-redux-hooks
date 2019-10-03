c19dc9d120552d9a775d2b54afc7670035187ec4
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2019-10-03 12:47:34 +0200

    Updates

---
+++ precache-manifest.908f60030ba4e7649f48e960fb6eed4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a330bb33a7c0fefa77fe458dc355900c",
+    "url": "/boilerplate-react-redux-hooks/index.html"
+  },
+  {
+    "revision": "c04bbf7cdd76275b1e53",
+    "url": "/boilerplate-react-redux-hooks/static/css/main.357a5197.chunk.css"
+  },
+  {
+    "revision": "d5927d18069d6a7d19e5",
+    "url": "/boilerplate-react-redux-hooks/static/js/2.155b0bc8.chunk.js"
+  },
+  {
+    "revision": "c04bbf7cdd76275b1e53",
+    "url": "/boilerplate-react-redux-hooks/static/js/main.efa2b36c.chunk.js"
+  },
+  {
+    "revision": "ab930fc4496a9cb37932",
+    "url": "/boilerplate-react-redux-hooks/static/js/runtime~main.ecd7f177.js"
+  }
+]);
